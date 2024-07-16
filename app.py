198759ec222c668ae641744142b6cffbb06278f6
JimNtantakas <dimitrisntantakas@gmail.com>
2024-07-17 01:26:36 +0300

    new pages

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
-from flask import Flask, render_template, url_for, request
-
+from flask import Flask, render_template, url_for, request, session
+from pymongo import MongoClient
 
 app = Flask(__name__)
 
+client = MongoClient('localhost', 27017)
+db = client["flask_database"]
+products = db['products']
+
 @app.route("/")
 def home():
-    return render_template("homepage.html")
+    if session.get("name"):
+        return render_template("homepage.html", x="Account", page="account")
+    else:
+        return render_template("homepage.html", x="Login", page="login")
 
 @app.route("/selling")
 def sell_page():
@@ -16,16 +23,24 @@ def sell_page():
 def submit():
     title = request.form.get("title")
     photos = request.form.get("photos")
-    categories = request.form.get("categories")
-    
+    categories = request.form.get("categories") 
     product = {"title": title,
                "photos": photos,
                "categories": categories
                }
-
+    products.insert_one(product)
     return render_template("submit.html")
 
 
+@app.route("/account.html")
+def account():
+    return render_template("account.html")
+
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+    
 
 
 
