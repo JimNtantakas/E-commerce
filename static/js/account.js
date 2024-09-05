d500ca0368be3fc023a42ed3e584852df9e43fb7
JimNtantakas <dimitrisntantakas@gmail.com>
2024-09-05 11:08:19 +0300

    added account info button and tab,and ratings button functionality

--- static/js/account.js
+++ static/js/account.js
@@ -4,15 +4,38 @@ const cart_btn = document.querySelector('#cart-btn');
 const ratings_btn = document.querySelector('#ratings-btn');
 const my_products_btn = document.querySelector('#published-products-btn');
 const logout_btn = document.querySelector("#logout-btn");
+const profile_btn = document.querySelector("#profile-btn")
+const checkout_btn = document.querySelector('#checkout-btn-link');
 
 const mainContent_div = document.querySelector('#main-content');
 
+
+profile_btn.addEventListener("click", function(){
+    hideCheckoutButton();
+    mainContent_div.innerHTML = '';
+    mainContent_div.classList.add('show');
+    const clonedUserDiv = document.querySelector('.user-div').cloneNode(true);
+    clonedUserDiv.classList.remove('hidden');
+    mainContent_div.appendChild(clonedUserDiv);
+});
+
+
+function showCheckoutButton() {
+    checkout_btn.classList.remove('hidden');
+}
+function hideCheckoutButton() {
+    checkout_btn.classList.add('hidden');
+}
+
+
 orders_btn.addEventListener("click", function(){
+    hideCheckoutButton();
     mainContent_div.innerHTML = '';
 });
 
 
 wishlist_btn.addEventListener("click", function(){
+    hideCheckoutButton();
     mainContent_div.innerHTML = '';
     this.disabled = true;
     fetch('/liked-products', {
@@ -76,6 +99,7 @@ wishlist_btn.addEventListener("click", function(){
         }
     })
     .finally( ()=> {
+        mainContent_div.classList.add('show');
         this.disabled = false;
     });
 });
@@ -143,6 +167,9 @@ cart_btn.addEventListener("click", function(){
                 list.appendChild(listItem);
             });
             mainContent_div.appendChild(list);
+            
+            showCheckoutButton()
+
         }
         else{
             const message = document.createElement('p');
@@ -152,17 +179,92 @@ cart_btn.addEventListener("click", function(){
         }
     })
     .finally( ()=> {
+        mainContent_div.classList.add('show');
         this.disabled = false;
     });
 });
 
 
 ratings_btn.addEventListener("click", function(){
+    hideCheckoutButton();
     mainContent_div.innerHTML = '';
+    this.disabled = true;
+    fetch('/rated-products', {
+        method: 'GET',
+        headers: {
+                'Content-Type': 'application/json'
+            }
+    })
+    .then(response => response.json())
+    .then(data => {
+        if (data.length !=0 ){
+            const list = document.createElement('ul');
+            list.classList.add('products-list');
+            data.forEach(product => {
+                const listItem = document.createElement('li');
+                const like_button = document.createElement('button');
+                like_button.id = `like-button-${product._id}`;
+                like_button.classList.add('like-button','liked');
+                like_button.type = 'submit';
+                const like_icon = document.createElement('i');
+                like_icon.classList.add('bx', 'bx-heart');
+                like_button.appendChild(like_icon);
+
+                const image_link = document.createElement('a');
+                image_link.href = `/products/${product._id}`;
+                const image = document.createElement('img');
+                image.classList.add('product-image');
+
+                const title = document.createElement('a');
+                title.text = product.title;
+                title.classList.add('product-title','product-details-link');
+                title.href = `/products/${product._id}`;
+
+                const rating_div = document.createElement('div')
+                rating_div.classList.add('rating-div');
+                const rating_icon = document.createElement('i');
+                rating_icon.classList.add('bx','bxs-star','account-star');
+                const rating_number = document.createElement('span')
+                rating_number.classList.add('rating-number-span')
+                rating_number.textContent = `${product.total_rating}/5`
+                rating_div.appendChild(rating_icon);
+                rating_div.appendChild(rating_number);
+                
+            
+            
+                if (product.photos.length > 0) {
+                    const photo_id = product.photos[0];  
+                    image.src = `/photo/${photo_id}`;
+                } else {
+                    image.alt = 'No image available';
+                }
+                
+                image_link.appendChild(image);
+                listItem.appendChild(like_button);
+                listItem.appendChild(image_link);
+                listItem.appendChild(title);
+                listItem.appendChild(rating_div);
+                listItem.classList.add('product-item');
+                list.appendChild(listItem);
+            });
+            mainContent_div.appendChild(list);
+        }
+        else{
+            const message = document.createElement('p');
+            message.classList.add('empty-message');
+            message.textContent = "Your wishlist is empty!"
+            mainContent_div.appendChild(message);
+        }
+    })
+    .finally( ()=> {
+        mainContent_div.classList.add('show');
+        this.disabled = false;
+    });
 });
 
 
 my_products_btn.addEventListener("click", function(){
+    hideCheckoutButton();
     mainContent_div.innerHTML = '';
     this.disabled = true;
     fetch('/published-products', {
@@ -233,6 +335,7 @@ my_products_btn.addEventListener("click", function(){
         }
     })
     .finally( ()=> {
+        mainContent_div.classList.add('show');
         this.disabled = false;
     });
 });
