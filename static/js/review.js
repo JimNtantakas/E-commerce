6b55a8760968119a04708fe8bb75172c12d5b978
JimNtantakas <dimitrisntantakas@gmail.com>
2024-09-05 11:10:31 +0300

    input responsive characters display

--- static/js/review.js
+++ static/js/review.js
@@ -2,7 +2,11 @@ const textarea = document.querySelector('.review-text');
 const characters_count = document.querySelector('.char-count');
 const max_characters = textarea.getAttribute('maxlength');
 
-textarea.addEventListener('input', ()=>{
-    const text_length = textarea.value.length;
+
+const updateCharacterCount = () => {
+    const text_length = textarea.value.replace(/\s+/g, '').length;
     characters_count.textContent = `${text_length}/${max_characters}`
-});
+};
+
+textarea.addEventListener('input', updateCharacterCount);
+updateCharacterCount();
